f1129a4ea11be9c1628ae23911730fb8dd15176e
leedow <644743991@qq.com>
2016-10-29 22:20:16 +0800

    侧栏菜单组件

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp 			= require('gulp');
+var less 			= require('gulp-less');
+var autoprefixer 	= require('gulp-autoprefixer');
+var watch 			= require('gulp-watch');
+var uglify			= require('gulp-uglify');
+var minifyCSS 		= require('gulp-minify-css');
+var webpack 		= require('gulp-webpack');
+var gutil 			= require('gulp-util');
+var ejs 			= require("gulp-ejs");
+var rev				= require('gulp-rev');
+
+gulp.task('css', function(){
+	watch('./style/**/*.less', function(){
+		gulp.src('./style/bone.less')
+			.pipe(less())
+			.on('error', function(err) {
+				gutil.log('Less Error!', err.message);
+				this.end();
+			})
+			//.pipe(rev())
+			.pipe(autoprefixer())
+			//.pipe(minifyCSS())
+			.pipe(gulp.dest('./build'))
+	});
+});
+
+gulp.task('default', function(){
+	gulp.run(['css']);
+});
