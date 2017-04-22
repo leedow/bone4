04ae2a2685eda54ee77f22ffcabcc2a36b6f87d4
leedow <644743991@qq.com>
2017-04-22 16:14:13 +0800

    add selector style

--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,7 @@ gulp.task('css', function(){
 			.pipe(gulp.dest('./build'))
 			.pipe(reload({stream: true}));
 
-
+			/*
 		gulp.src('./style/bone-pc.less')
 			.pipe(less())
 			.on('error', function(err) {
@@ -59,7 +59,7 @@ gulp.task('css', function(){
 			//.pipe(rev())
 			.pipe(autoprefixer())
 			//.pipe(minifyCSS())
-			.pipe(gulp.dest('./build'))
+			.pipe(gulp.dest('./build'))*/
 
 		gulp.src('./style/bone-mobile.less')
 			.pipe(less())
@@ -84,6 +84,8 @@ gulp.task('css', function(){
 			//.pipe(minifyCSS())
 			.pipe(gulp.dest('./build'))
 
+
+			/*
 		gulp.src('./style/bone-mobile-x2.less')
 			.pipe(less())
 			.on('error', function(err) {
@@ -93,7 +95,7 @@ gulp.task('css', function(){
 			//.pipe(rev())
 			.pipe(autoprefixer())
 			//.pipe(minifyCSS())
-			.pipe(gulp.dest('./build'))
+			.pipe(gulp.dest('./build'))*/
 
 
 
@@ -108,7 +110,7 @@ gulp.task('css', function(){
 				//.pipe(minifyCSS())
 				.pipe(gulp.dest('./build'))
 
-
+				/*
 			gulp.src('./style/bone-all-darkblue.less')
 				.pipe(less())
 				.on('error', function(err) {
@@ -118,7 +120,7 @@ gulp.task('css', function(){
 				//.pipe(rev())
 				.pipe(autoprefixer())
 				//.pipe(minifyCSS())
-				.pipe(gulp.dest('./build'))
+				.pipe(gulp.dest('./build'))*/
 	});
 });
 
